a25e1bc702049c0b0a21ce9dd664bc4ea1d0f8ac
CaritoDaSilvaMeli <carolina.dasilva@mercadolibre.cl>
2023-05-29 21:51:51 -0400

    Clase de autenticación y autorización

--- mern/class11/todo-list-app/server/routes/task.routes.js
+++ mern/class11/todo-list-app/server/routes/task.routes.js
@@ -1,10 +1,11 @@
+const { authenticate } = require("../config/jwt.config");
 const { getAllTasks, createTask, updateTask, deleteTask, getOneTask } = require("../controllers/task.controllers");
 
 
 module.exports = (app) => {
-    app.get('/api/task', getAllTasks);
-    app.get('/api/task/:id', getOneTask);
-    app.post('/api/task', createTask);
-    app.put('/api/task/:id', updateTask);
-    app.delete('/api/task/:id', deleteTask);
+    app.get('/api/task',authenticate ,getAllTasks);
+    app.get('/api/task/:id', authenticate, getOneTask);
+    app.post('/api/task', authenticate, createTask);
+    app.put('/api/task/:id', authenticate, updateTask);
+    app.delete('/api/task/:id', authenticate, deleteTask);
 }
